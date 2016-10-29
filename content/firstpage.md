836f18643fdb5c057a049091b9580903d09d2ba6
Swapnil Jariwala <swapnil.jariwala@gmail.com>
2016-10-29 18:09:37 +0530

    initialized repo

---
+++ content/firstpage.md
@@ -0,0 +1,13 @@
+Title:My first page
+Date: 2016-10-29
+Tags:Python, ML
+Category: Blog
+The title
+=========
+The code
+--------
+```python
+def somefunc():
+    pass
+```
+
---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'swapniljariwala'
+SITENAME = u'The Lab'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = ('swapniljariwala.github.io')
+
+# Social widget
+SOCIAL = [ ('Linkedin', 'https://in.linkedin.com/in/swapnilj') ]
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
